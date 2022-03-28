9de48bb6c2f11e3bb33bbbb21fc3a8f0f403ec83
Gabriel Wright <gabriel.wright@ryerson.ca>
2022-03-28 10:37:15 -0400

    removed extra script files

--- scripts/slide-out-nav.js
+++
@@ -1,9 +0,0 @@
-// // TARGET ELEMENTS
-// const menu = document.querySelector(".open-sidebar-menu");
-
-// // FUNCTIONS
-
-// // EVENT LISTENERS
-// menu.addEventListener("click", function () {
-// 	menu.classList.toggle(".slide-in-nav");
-// });
